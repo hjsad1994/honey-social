97860a79a0b5ebb72a83675899eba7a6d763fb4c
hjsad1994 <trantai306@gmail.com>
2025-04-13 15:55:15 +0700

    update profile controller

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,7 +1,20 @@
 import User from "../model/userModel.js"
 import bcrypt from "bcryptjs"
 import GenerateTokenAndSetCookie from "../utils/helpers/GenerateTokenAndSetCookie.js"
-import generateTokenAndSetCookie from "../utils/helpers/GenerateTokenAndSetCookie.js"
+
+const getUserProfile = async (req, res) => {
+    const { username } = req.params
+    try {
+        const user = await User.findOne({ username }).select("-password").select("-updateAt") // exclude password from user object
+        if (!user) {
+            return res.status(404).json({ message: "user not found" })
+        }
+        res.status(200).json({user})
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+        console.log("error in getUserProfile controller", err.message)
+    }
+}
 
 
 // dang ky
@@ -83,7 +96,7 @@ const followUnfollowUser = async (req, res) => {
         const { id } = req.params
         const userToModify = await User.findById(id)
         const CurrentUser = await User.findById(req.user._id)
-        if (id === req.user._id) {
+        if (id === req.user._id.toString()) {
             return res.status(400).json({ message: "you cannot follow/unfollow yourself" })
         }
         if (!userToModify || !CurrentUser) {
@@ -107,4 +120,35 @@ const followUnfollowUser = async (req, res) => {
         console.log("error in followUnfollowUser controller", err.message)
     }
 }
-export { signupUser, loginUser, logoutUser, followUnfollowUser }
+const updateUser = async (req, res) => {
+    const { name, email, username, password, profilePic, bio } = req.body
+    const userId = req.user._id
+    try {
+        let user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({ message: "user not found" })
+        }
+        if (req.params.id !== userId.toString()) {
+            return res.status(400).json({ message: "you are not authorized to update this user" })
+        }
+        if (password) {
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(password, salt)
+            user.password = hashedPassword
+        }
+        user.name = name || user.name
+        user.email = email || user.email
+        user.username = username || user.username
+        user.profilePic = profilePic || user.profilePic
+        user.bio = bio || user.bio
+        user = await user.save()
+        res.status(200).json({ message: "profuile updated successfully", user })
+
+
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+        console.log("error in updateUser controller", err.message)
+    }
+}
+
+export { signupUser, loginUser, logoutUser, followUnfollowUser, updateUser, getUserProfile }
